e4b483a9e87a2a0e6cd2ba30bf23c4928c2ffd3c
eric.promislow <eric.promislow@78f59cab-db50-0410-81a3-836bb803b358>
2009-03-30 04:53:10 +0000

    Version 0.8.0: Implement an actual Preferences box with options on launch char, close-on-return, and also show-statusbar-icon and show-menuitem.

--- trunk/build.sh
+++ trunk/build.sh
@@ -6,6 +6,8 @@ cat << ZAK | zip tabhunter.jar -@
 ./content/selectTabDialog.xul
 ./content/tabhunter.js
 ./content/tabhunterOverlay.xul
+./content/prefs.xul
+./content/prefs.js
 ./locale/en-US/tabhunter.dtd
 ./locale/en-US/strings.properties
 ./skin/martini-16x16.png
@@ -14,6 +16,6 @@ cat << ZAK | zip tabhunter.jar -@
 ZAK
 # zip -r tabhunter.jar content locale skin
 cd ..
-T=tabhunter-0.7.6.xpi
+T=tabhunter-0.8.0.xpi
 rm -f $T
 zip $T  chrome.manifest install.rdf chrome/tabhunter.jar defaults/preferences/prefs.js
