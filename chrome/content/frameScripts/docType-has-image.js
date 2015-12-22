db72dfe564bfd9ff16115d99f3d2b49886bc50a2
Eric Promislow <eric.promislow@gmail.com>
2015-12-21 18:11:30 -0800

    remove extra log stmts

--- chrome/content/frameScripts/docType-has-image.js
+++ chrome/content/frameScripts/docType-has-image.js
@@ -2,15 +2,12 @@
 
 var consoleService = Components.classes["@mozilla.org/consoleservice;1"]
     .getService(Components.interfaces.nsIConsoleService);
-//consoleService.logStringMessage("-Loading docType-has-image.js...");
 
  handleMessage = function(msgData) {
     var consoleService = Components.classes["@mozilla.org/consoleservice;1"]
     .getService(Components.interfaces.nsIConsoleService);
     try {
-       //        consoleService.logStringMessage("RRR: -doStuff(msgData.data: " + Object.keys(msgData.data).join(" ") + ")");
         doStuff(msgData.data);
-	//        consoleService.logStringMessage("RRR: +doStuff()")
     } catch(e) {
         consoleService.logStringMessage("Bad happened: " + e);
     }
@@ -18,16 +15,10 @@ var consoleService = Components.classes["@mozilla.org/consoleservice;1"]
 
 function doStuff(data) {
     var consoleService = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
-    //consoleService.logStringMessage("content.contentDocument: " + content.contentDocument);
-    //    consoleService.logStringMessage("RRR: content.document: " + content.document);
-    //    consoleService.logStringMessage("RRR: content.document.contentType: " + content.document.contentType);
     // Send all data back, along with the result
     data.hasImage = content.document.contentType.indexOf("image/") >= 0;
-    if (data.location == "about:blank") {
-       // Keep waiting
     data.location = content.document.location.toString();
     consoleService.logStringMessage("RRR: doc FS got location: " + data.location.substr(0, 40) + ", hasImage:" + data.hasImage);
-    //consoleService.logStringMessage("RRR: hasImage: " + data.hasImage);
     sendAsyncMessage("tabhunter@ericpromislow.com:docType-has-image-continuation", data);
 }
 
