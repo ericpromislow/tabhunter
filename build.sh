35a5ddb63892a153b928ef71d276878864267621
Eric Promislow <eric.promislow@gmail.com>
2015-12-24 11:17:32 -0800

    keep the jar file, put jsm in a separate spot

--- build.sh
+++ build.sh
@@ -1,25 +1,26 @@
-T=tabhunter-1.0.14.xpi
+cd chrome
+rm -f tabhunter.jar
+cat << ZAK | zip tabhunter.jar -@
+./content/selectTabDialog.js
+./content/sessionTrack.js
+./content/selectTabDialog.xul
+./content/tabhunter.js
+./content/tabhunterOverlay.xul
+./content/prefs.xul
+./content/prefs.js
+./content/frameScripts/browser-window-focus.js
+./content/frameScripts/docType-has-image.js
+./content/frameScripts/search-next-tab.js
+./locale/en-US/tabhunter.dtd
+./locale/en-US/tabhunter.properties
+./locale/en-US/strings.properties
+./skin/martini-16x16.png
+./skin/martini-24x24.png
+./skin/tabhunter-install.png
+./skin/tabhunter.css
+ZAK
+# zip -r tabhunter.jar content locale skin
+cd ..
+T=tabhunter-1.0.15.xpi
 rm -f $T
-zip $T  chrome.manifest install.rdf chrome/tabhunter.jar defaults/preferences/prefs.js \
-chrome/content/selectTabDialog.js \
-chrome/content/sessionTrack.js \
-chrome/content/selectTabDialog.xul \
-chrome/content/tabhunter.js \
-chrome/content/tabhunter.jsm \
-chrome/content/tabhunterOverlay.xul \
-chrome/content/prefs.xul \
-chrome/content/prefs.js \
-chrome/content/frameScripts/browser-window-focus.js \
-chrome/content/frameScripts/docType-has-image.js \
-chrome/content/frameScripts/search-next-tab.js \
-chrome/locale/en-US/tabhunter.dtd \
-chrome/locale/en-US/tabhunter.properties \
-chrome/locale/en-US/strings.properties \
-chrome/skin/martini-16x16.png \
-chrome/skin/martini-24x24.png \
-chrome/skin/tabhunter-install.png \
-chrome/skin/tabhunter.css
-
-
-# Ignore this line
-# Ignore this line
+zip $T  chrome.manifest install.rdf chrome/tabhunter.jar defaults/preferences/prefs.js modules/tabhunter.jsm
