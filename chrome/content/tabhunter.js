65da515f9b1964a5c242c99d9046b749135b8560
Eric Promislow <eric.promislow@gmail.com>
2015-12-24 11:35:04 -0800

    loading from the overlay just doesn't work.  Go back to listening to load/unload events (temporarily)

--- chrome/content/tabhunter.js
+++ chrome/content/tabhunter.js
@@ -9,13 +9,18 @@ function tabhunter_global_load(event) {
    alert("+ imported th.jsm");
    ep_extensions.tabhunter.onload(event);
    alert("+ did th.onload");
+   window.removeEventListener("load", tabhunter_global_load, false);
+   window.addEventListener("unload", tabhunter_global_unload, false);
    } catch(ex) {
       alert("failed to load tabhunter: " + ex);
    }
 }
 
 function tabhunter_global_unload(event) {
-   ep_extensions.tabhunter.onunload(event);
+   alert("**** >> tabhunter_global_unload");
+   // ep_extensions.tabhunter.onunload(event);
 }
+
+window.addEventListener("load", tabhunter_global_load, false);
 
 alert("done loading tabhunter.js");
