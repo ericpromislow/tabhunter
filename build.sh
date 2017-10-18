5aea28bcfedbc81e513b5c6f8748ff80c9257822
Eric Promislow <eric.promislow@gmail.com>
2017-10-17 19:59:20 -0700

    move from jquery 3.1.1 to 3.2.1 per mozilla's demands

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-zip -r tabhunter-2.0.0.zip LICENSE.txt  README.md _locales/ icons/ manifest.json popup/ -x '*~'
+zip -r tabhunter-2.0.1.zip LICENSE.txt  README.md _locales/ icons/ manifest.json popup/ -x '*~'
