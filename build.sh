84234136b5785c8820ed2f2bdd67baada34f97cd
Eric Promislow <eric.promislow@gmail.com>
2018-03-11 17:43:41 -0700

    version 2.0.4 stuff

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-version=${1:-2.0.3}
-zip -r tabhunter-${version}.zip LICENSE.txt  README.md key.pem \
+version=${1:-2.0.4}
+zip -r tabhunter-${version}.zip LICENSE.txt  README.md \
    _locales/ icons/ manifest.json \
-   popup/tabhunter.{css,html,js} \
+   popup/{tabhunter,prefs}.{css,html,js} \
    popup/jquery-3.2.1.min.js popup/browser-polyfill.min.js \
    -x '*~'
+
+#  key.pem
