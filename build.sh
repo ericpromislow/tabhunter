e0afd067790867068070bf413d7e01d6454a4c8e
Eric Promislow <eric.promislow@gmail.com>
2018-04-07 12:34:13 -0700

    change font color to show dups

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-version=${1:-2.0.8}
+version=${1:-2.0.9}
 zip -r tabhunter-${version}.zip LICENSE.txt  README.md \
    _locales/ icons/ manifest.json \
    popup/{tabhunter,prefs}.{css,html,js} \
-   popup/jquery-3.2.1.min.js popup/browser-polyfill.min.js \
-   -x '*~'
+   popup/jquery-3.2.1.min.js popup/browser-polyfill.min.js
 
 #  key.pem
